6c11fa5d21037acc20d4c3f06f0ef30a3eca9d88
Matt Groth <mgroth49@gmail.com>
2020-07-25 19:44:19 -0400

    auto-commit 4 singularity

--- work.py
+++ work.py
@@ -21,6 +21,7 @@ FLAGS = margparse(
 )
 from lib.boot import nn_init_fun
 tf = nn_init_fun.setupTensorFlow(FLAGS)
+breakpoint()
 if FLAGS.expid == '0' and (not FLAGS.gen) and (not FLAGS.deletenorms): nn_init_fun.NRC_IS_FINISHED()
 setup_logging(verbose=FLAGS.verbose)
 if FLAGS.tic is not None: setTic(FLAGS.tic * 1000)
