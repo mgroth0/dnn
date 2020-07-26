72875428b04b175e5063f64f971e3ba856fbdaf3
Matt Groth <mgroth49@gmail.com>
2020-07-26 14:07:19 -0400

    auto-commit 4 singularity

--- arch/model_wrapper.py
+++ arch/model_wrapper.py
@@ -22,7 +22,10 @@ def chain_predict(nets, pp, inputs):
                 assert rimg.shape == (3, n.HEIGHT_WIDTH, n.HEIGHT_WIDTH)  # TEMP-DEBUG
                 vs += n.predict(rimg)
             else:
-                assert img.shape == (n.HEIGHT_WIDTH, n.HEIGHT_WIDTH, 3)  # TEMP-DEBUG
+                try:
+                    assert img.shape == (n.HEIGHT_WIDTH, n.HEIGHT_WIDTH, 3)  # TEMP-DEBUG
+                except AssertionError:
+                    breakpoint()
                 vs += n.predict(img)
     return tuple([vs.mat for vs, n in vs_n])
 
