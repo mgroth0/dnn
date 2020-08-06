dc611b9381c1b512ab280a4622f2750c568f6ac2
Matt Groth <mgroth49@gmail.com>
2020-08-05 20:27:44 -0400

    small fix

--- build_imagenet_data.py
+++ build_imagenet_data.py
@@ -87,7 +87,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from dataclasses import dataclass
 
 from datetime import datetime
 import os
