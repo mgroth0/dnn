4f23cd1fd4228ce48ec65e68e09ee449b2ba9b32
Matt Groth <mgroth49@gmail.com>
2020-08-14 03:00:52 -0400

    auto-commit 4 singularity

--- arch/model_wrapper.py
+++ arch/model_wrapper.py
@@ -171,6 +171,8 @@ def simple_predict(net: ModelWrapper, pp, inputs, *, length):
             self.counter = 0
             self.cache = {}
         def __getitem__(self, index):
+            log(f'trying to get index {index}')
+            log(f'current indices range from {min(list(self.cache.keys()))} to {max(list(self.cache.keys()))}')
             if index in self.cache:
                 r = self.cache[index]
                 del self.cache[index]
