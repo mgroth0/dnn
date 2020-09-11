7f9977493dde5dad0ef34a85ff24c4a792719941
Matt Groth <mgroth49@gmail.com>
2020-09-11 01:05:56 -0400

    auto-commit 4 singularity

--- rsa_for_darius.py
+++ rsa_for_darius.py
@@ -17,7 +17,7 @@ SANITY_FILE = File('/Users/matt/Desktop/forMattActivs.mat')
 # on openmind 10 took ~400 sec
 # N_PER_CLASS = 100
 # N_PER_CLASS = 15 # took 524 sec
-N_PER_CLASS = 20  # took 687, with multiprocess took 363!
+N_PER_CLASS = 20  # took 687, with multiprocess took 363 (and again, 362)! (56 CPUS)
 
 import multiprocessing
 print(f'NUM CPUS: {multiprocessing.cpu_count()}')
