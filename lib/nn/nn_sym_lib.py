5475361c242b4a1e8d43ff006f643853bf07980f
Matt Groth <mgroth49@gmail.com>
2020-12-19 18:57:40 -0500

    auto-commit 4 singularity

--- lib/nn/nn_sym_lib.py
+++ lib/nn/nn_sym_lib.py
@@ -1,8 +1,8 @@
 from mlib.boot.mlog import err
 def reduced_label(label):
-    if label.startswith('S') and len(label) in [2, 3, 4]:
+    if label == 'dog' or (label.startswith('S') and len(label) in [2, 3, 4]):
         return 0
-    elif label.startswith('N') and len(label) in [3, 4, 5]:
+    elif label == 'cat' or (label.startswith('N') and len(label) in [3, 4, 5]):
         return 1
     else:
         err('do not know reduced label for ' + label)
