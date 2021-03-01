af1be339efa17ccb0ca8cc6399c1d86388fca054
Matt Groth <mgroth49@gmail.com>
2021-02-28 20:09:34 -0500

    auto-commit 4 singularity

--- comp_signals.py
+++ comp_signals.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 
 import json
+from time import time
 
-from time import sleep, time
+from mlib.boot import log
 
 @dataclass
 class CompSignaler:
@@ -13,17 +14,18 @@ class CompSignaler:
 
     def send_start_signal(self):
         self._start_time = time()
-        return self._send_signal(start_time = self._start_time,end_time=None)
+        rrr = self._send_signal(start_time=self._start_time, end_time=None)
+        log('SENT START SIGNAL')
+        return rrr
 
     def send_end_signal(self):
-        return self._send_signal(start_time=self._start_time,end_time=time())
+        return self._send_signal(start_time=self._start_time, end_time=time())
 
-    def _send_signal(self,start_time,end_time):
-        with open(f'/Users/matt/Desktop/registered/signal/comp/{time()}.json','w') as f:
+    def _send_signal(self, start_time, end_time):
+        with open(f'/Users/matt/Desktop/registered/signal/comp/{time()}.json', 'w') as f:
             f.write(json.dumps({
                 'id'     : self.id,
                 'started': start_time,
                 'ended'  : end_time
                 # 'message': 'STARTED'
             }))
-
