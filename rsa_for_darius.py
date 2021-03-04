5e1634bf745ba7604d3a72ec92069533d3d7752a
Matt Groth <mgroth49@gmail.com>
2021-03-04 16:56:41 -0500

    auto-commit 4 singularity

--- rsa_for_darius.py
+++ rsa_for_darius.py
@@ -435,13 +435,13 @@ def debug_process(fd, scores, result_folder, net, block_len, arch, size, plot,fu
         'p_across_ns': p_across_ns
     })
 
-    # try:
+    try:
 
-    similarity_NS_std = np.std(similarity_NS_flat)
-    similarity_S_stf = np.std(similarity_S_flat)
-    dissimilarity_across_std = np.std(sim_across_flat)
-    # except:
-    #     breakpoint()
+        similarity_NS_std = np.std(similarity_NS_flat)
+        similarity_S_stf = np.std(similarity_S_flat)
+        dissimilarity_across_std = np.std(sim_across_flat)
+    except:
+        breakpoint()
 
     if plot == 'AC':
         scores[arch][size] = dissimilarity_across
