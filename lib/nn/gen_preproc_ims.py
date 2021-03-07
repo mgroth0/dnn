8acdc5006001fe8d1b009f69e07cb27f60546b34
Matt Groth <mgroth49@gmail.com>
2021-03-07 01:44:55 -0500

    auto-commit 4 singularity

--- lib/nn/gen_preproc_ims.py
+++ lib/nn/gen_preproc_ims.py
@@ -352,6 +352,7 @@ class PreDataset:
                     if break_outer:
                         break
 
+        breakpoint()
         return examples
     def prep(self, HW, pp_type):
         # RANDOMNESS HERE
