bd663243c7a371a236fb9a165a13b29fa8f0fa03
Matthew Groth <matt@Mac.local>
2021-03-18 00:03:51 -0400

    auto-commit 4 singularity

--- boot.py
+++ boot.py
@@ -1,7 +1,12 @@
 from mlib.boot import exec_proj
+exec_proj.dummy = None
 import os
+os.dummy = None
 import mlib.file
 mlib.file.dummy = 'please stop circular imports'
-exec_proj.exec_proj(
-    os.path.basename(os.getcwd())
-)
+# exec_proj.exec_proj(
+#     os.path.basename(os.getcwd())
+# )
+# DEBUG
+import dnn
+dnn.dummy = None
