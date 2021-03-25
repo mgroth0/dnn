d4fd3076e5001cdab441479d70580b8f5d9a31eb
Matthew Groth <matt@Mac.local>
2021-03-24 22:54:32 -0400

    auto-commit 4 singularity

--- boot_remote.py
+++ boot_remote.py
@@ -2,7 +2,6 @@ if __name__ == '__main__':
     import boot
     boot.take_tic_from_sysargv()
     import pickle
-    breakpoint()
     with open('_remote_scope.pkl', 'rb') as scope:
         globals().update(pickle.load(scope))
     with open('_remote_script.py', 'r') as pyscript:
